8caa68a9a4ac312efe3b6420b5a63d485b6fd6d5
udaykant <udaykant@thoughtworks.com>
2014-01-24 12:06:06 +0530

    negative number handled

--- src/library/ExpEval.java
+++ src/library/ExpEval.java
@@ -1,17 +1,25 @@
 package library;
 
+import com.sun.org.apache.regexp.internal.recompile;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExpEval {
     public int evaluator(String expression) {
         List<Integer> Operands = new ArrayList<Integer>();
+        if (0 == compareParentheses(expression)) {
+            System.out.println("wrong input");
+            return 0;
+        }
+        expression = handleSpaces(expression);
 
         if (expression.contains("(")) {
             expression = evaluageParentheses(expression).trim();
-            if(expression.contains(")")){
-                int index= expression.indexOf(")");
-                expression = expression.substring(0,index-1);
+            expression = handleSpaces(expression);
+            if (expression.contains(")")) {
+                int index = expression.indexOf(")");
+                expression = expression.substring(0, index - 1);
             }
         }
         String[] elements = expression.split(" ");
@@ -37,16 +45,20 @@ public class ExpEval {
     private String evaluageParentheses(String expression) {
         int result = 0;
         List<Integer> Operands = new ArrayList<Integer>();
-        if (expression.contains("(")) {
+
+
+        if (expression.contains("("))
+
+        {
             int closeBracketIndex = 0;
             int openBracketIndex = 0;
             openBracketIndex = expression.indexOf("(");
-            closeBracketIndex = expression.indexOf(")",closeBracketIndex);
+            closeBracketIndex = expression.indexOf(")", closeBracketIndex);
             String innerExpression = expression.substring(openBracketIndex, closeBracketIndex + 1);
             String expressionWithoutBracktes = expression.substring(openBracketIndex + 1, closeBracketIndex);
-            if(expressionWithoutBracktes.contains("(")){
-                int openBracketsIndex= expressionWithoutBracktes.indexOf("(");
-                expressionWithoutBracktes= expressionWithoutBracktes.substring(openBracketsIndex+1,expressionWithoutBracktes.length());
+            if (expressionWithoutBracktes.contains("(")) {
+                int openBracketsIndex = expressionWithoutBracktes.indexOf("(");
+                expressionWithoutBracktes = expressionWithoutBracktes.substring(openBracketsIndex + 1, expressionWithoutBracktes.length());
             }
             String[] elements = expressionWithoutBracktes.split(" ");
             List<String> Operators = getOperatorsAndOperands(Operands, elements);
@@ -55,10 +67,15 @@ public class ExpEval {
             expression = evaluageParentheses(expression);
 
         }
+        if (expression.contains(")")) {
+            expression = expression.replaceAll("\\)", "");
+        }
         return expression;
     }
 
     private int evaluate(List<Integer> Operands, List<String> Operators) {
+        if (Operands.size() < 2)
+            return Operands.get(0);
         for (int i = 0; i < Operators.size(); i++) {
             int first = Operands.get(i);
             int result = 0;
@@ -77,4 +94,37 @@ public class ExpEval {
         int result = Operands.get(Operands.size() - 1);
         return result;
     }
+
+    public int compareParentheses(String expression) {
+        int openBracketsCount = 0;
+        int closeBracketsCount = 0;
+        String[] elements = expression.split("");
+        for (String element : elements) {
+            if (element.contains("(")) {
+                openBracketsCount++;
+            }
+            if (element.contains(")")) {
+                closeBracketsCount++;
+            }
+        }
+        if (openBracketsCount == closeBracketsCount)
+            return 1;
+        return 0;
+
+    }
+
+    private String handleSpaces(String expression) {
+        expression = expression.replaceAll(" +", "");
+        expression = expression.replaceAll("\\+", " + ")
+                .replaceAll("\\-", " - ")
+                .replaceAll("/", " / ")
+                .replaceAll("\\*", " * ")
+                .replaceAll("\\^", " ^ ")
+                .replaceAll("\\+   ", " + ")
+                .replaceFirst("^ - ", "-").replaceAll("\\( - ", "(-")
+                .replaceAll("\\+  - ", "- ");
+        System.out.println(expression);
+        return expression;
+    }
+
 }
--- test/library/ExpEvalTest.java
+++ test/library/ExpEvalTest.java
@@ -5,146 +5,207 @@ import org.junit.Test;
 
 public class ExpEvalTest {
     @Test
-    public void evaluater_should_add_one_and_two_result_three(){
+    public void evaluater_should_add_one_and_two_result_three() {
         int expected = 3;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("1 + 2");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluater_should_substract_four_from_eight(){
+    public void evaluater_should_substract_four_from_eight() {
         int expected = 4;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("8 - 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluater_should_multiply_four_from_eight(){
+    public void evaluater_should_multiply_four_from_eight() {
         int expected = 32;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("8 * 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluater_should_divide_eight_from_four(){
+    public void evaluater_should_divide_eight_from_four() {
         int expected = 2;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("8 / 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluater_should_square_from_four(){
+    public void evaluater_should_square_from_four() {
         int expected = 256;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("4 ^ 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluater_should_add_three_operand_and_two_operator(){
+    public void evaluater_should_add_three_operand_and_two_operator() {
         int expected = 4;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("5 - 4 + 3");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void multiply_two_three_and_four_get_result_twenty_four(){
+    public void multiply_two_three_and_four_get_result_twenty_four() {
         int expected = 24;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("2 * 3 * 4");
         Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void multiply_two_three_and_substract_get_result_twenty_four(){
+    public void multiply_two_three_and_substract_get_result_twenty_four() {
         int expected = 2;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("2 * 3 - 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void divide_two_by_three_and_subtract_four_from_result(){
+    public void divide_two_by_three_and_subtract_four_from_result() {
         int expected = -4;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("2 / 3 - 4");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void add_two_and_three_with_single_parentheses(){
+    public void add_two_and_three_with_single_parentheses() {
         int expected = 5;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 + 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void subtract_two_and_three_with_single_parentheses(){
+    public void subtract_two_and_three_with_single_parentheses() {
         int expected = -1;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 - 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void multiply_two_and_three_with_single_parentheses(){
+    public void multiply_two_and_three_with_single_parentheses() {
         int expected = 6;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 * 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void add_two_and_three_and_five_and_six_with_multiple_parentheses(){
+    public void add_two_and_three_and_five_and_six_with_multiple_parentheses() {
         int expected = 13;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 + 3)+(5 + 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void substract_two_and_three_and_add_five_and_six_with_multiple_parentheses(){
+    public void substract_two_and_three_and_add_five_and_six_with_multiple_parentheses() {
         int expected = 7;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 - 3)+(5 + 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void multiply_two_and_three_and_add_five_and_six_with_multiple_parentheses(){
+    public void multiply_two_and_three_and_add_five_and_six_with_multiple_parentheses() {
         int expected = 14;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 * 3)+(5 + 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void divide_two_and_three_and_add_five_and_six_with_multiple_parentheses(){
+    public void divide_two_and_three_and_add_five_and_six_with_multiple_parentheses() {
         int expected = 8;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 / 3)+(5 + 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluate_two_to_power_three(){
+    public void evaluate_two_to_power_three() {
         int expected = 8;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("(2 ^ 3)");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void evaluate_expression_with_nested_parentheses(){
+    public void evaluate_expression_with_nested_parentheses() {
         int expected = 10;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("((2 + 3)+(1 * 5))");
-        Assert.assertEquals(expected,actual);
-    } 
-     @Test
-    public void multiply_two_and_three_and_multiply_one_and_five_with_nested_brackets(){
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void multiply_two_and_three_and_multiply_one_and_five_with_nested_brackets() {
         int expected = 30;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("((2 * 3)*(1 * 5))");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void divide_5_from_2_with_nested_parentheses(){
+    public void divide_5_from_2_with_nested_parentheses() {
         int expected = 2;
         ExpEval exp_eval = new ExpEval();
         int actual = exp_eval.evaluator("((5 / 2))");
-        Assert.assertEquals(expected,actual);
+        Assert.assertEquals(expected, actual);
     }
 
+    @Test
+    public void validate_user_input() {
+        int expected = 0;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("((5 / 2)");
+        Assert.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void evaluator_should_take_minus_two_and_plus_eight_and_should_give_answer_six() {
+        int expected = 10;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("2+8");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evaluate_expression_with_negative_values() {
+        int expected = 6;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("-2+8");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evaluate_expression_minus_eight() {
+        int expected = -8;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("(-8)");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evaluate_expression_plus_five(){
+        int expected = 8;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("(+8)");
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void evaluate_expression_five_plus_minus_three(){
+        int expected = 2;
+        ExpEval exp_eval = new ExpEval();
+        int actual = exp_eval.evaluator("5+(-3)");
+        Assert.assertEquals(expected, actual);
+    }
 
 }
